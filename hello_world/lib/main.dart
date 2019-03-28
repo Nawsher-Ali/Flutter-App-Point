7006dec43fc72d8da71dcc12911012056db2156c
Nawsher Ali <nawsher321@gmail.com>
2019-03-28 15:23:07 +0600

    add hello World project

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';//permision to create material widget
+
+// this method is entry point of app
+void main(){
+  runApp(
+   Center(
+     child:Text('Hello World',textDirection:TextDirection.ltr)
+   )
+  );
+}
