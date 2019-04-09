46c2215848d4d1ed51ec7dc8bc5603178fa885b9
Nawsher Ali <40803579+Nawsher-Ali@users.noreply.github.com>
2019-04-09 08:24:09 +0600

    Update main.dart

--- 13_card_widget_demo/lib/main.dart
+++ 13_card_widget_demo/lib/main.dart
@@ -6,47 +6,61 @@ void main() {
     home: MyApp(),
     debugShowCheckedModeBanner: false,
   ));
-}
+}// main method end
 
 class MyApp extends StatelessWidget {
+  
   @override
   build(BuildContext context) {
     return Scaffold(
+      
         appBar: AppBar(
           title: Text('Exploring Cards Widget'),
           backgroundColor: Colors.redAccent,
-        ),
+        ),//AppBar
+      
         body: ListView(
           padding: EdgeInsets.all(15),
           children: <Widget>[
+            
+            // Card 1 abd padding
             Padding(
               padding:EdgeInsets.only(top:10,bottom:8,left: 2),
               child: Text('First Card')),
             getCard('assets/c.jpg'),
-
+           
+            
+           // Card 2 abd padding
            Padding(
               padding:EdgeInsets.only(top:10,bottom:8,left: 2),
               child: Text('Second Card')),
             getCard('assets/d.jpg'),
-
+            
+            
+            // Card 3 abd padding
             Padding(
               padding:EdgeInsets.only(top:10,bottom:8,left: 2),
               child: Text('Third Card')),
             getCard('assets/j.jpg'),
-
+            
+            
+            // Card 4 abd padding
             Padding(
               padding:EdgeInsets.only(top:10,bottom:8,left: 2),
               child: Text('Fourth Card')),
             getCard('assets/l.jpg'),
-
+            
+            
+            // Card 5 abd padding
             Padding(
               padding:EdgeInsets.only(top:10,bottom:8,left: 2),
               child: Text('Fifth Card')),
             getCard('assets/m.jpg'),
+            
           ],
-        )
-      );
-  }
+        )//ListView
+      );//Scaffold
+  }// build method
 
 
   // this method is used for build Card Widget
@@ -62,15 +76,19 @@ class MyApp extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
+          
           Image.asset(imageName),
+          
           Padding(
             padding: EdgeInsets.only(left: 10,top:15),
             child:  Text('Awesome Food'),
-          ),
+          ),//Padding
+          
           Padding(
             padding: EdgeInsets.only(left: 10,bottom:10),
             child: Text('Price : 10\$')
-          ),
+          ),//Padding
+          
           Padding(
             padding: EdgeInsets.only(left: 10,bottom:10),
             child: Row(
@@ -79,13 +97,13 @@ class MyApp extends StatelessWidget {
                 Icon(Icons.share,color:Colors.redAccent),
                 Icon(Icons.star,color:Colors.redAccent),
               ]
-            )
-          )
+           )//Row
+         )//Padding
        ]
-      ),
+      ),//Column
 
-    );
-  }
+    );//Card
+  }// getCard method end 
 
 
 }
