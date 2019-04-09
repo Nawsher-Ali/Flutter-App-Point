1bad53debcf683161c7a943a2cfbedf6cd1b4113
Nawsher Ali <40803579+Nawsher-Ali@users.noreply.github.com>
2019-04-09 08:12:48 +0600

    Update main.dart

--- 12_listview_widget_demo/lib/main.dart
+++ 12_listview_widget_demo/lib/main.dart
@@ -8,15 +8,18 @@ void main(){
       debugShowCheckedModeBanner:false,
     )
   );
-}
+}// main method end
 
 class MyApp extends StatelessWidget{
+  
   @override
   build(BuildContext context){
     return Scaffold(
-      appBar:AppBar(title:Text('ListView Widget Exploring'),
-      backgroundColor:Colors.blue[900]
-      ),
+      
+      appBar:AppBar(
+       title:Text('ListView Widget Exploring'),
+       backgroundColor:Colors.blue[900]
+      ),//AppBar
 
       body:ListView(
         children: <Widget>[
@@ -34,11 +37,12 @@ class MyApp extends StatelessWidget{
           getListTile('Md Nahid', 'nahid@gmail.com',Colors.blue),
 
         ],
-      )
-    );
-  }
+      )//ListView
+    );//Scaffold
+  }//build method
 
-
+  
+  // get ListTile
   getListTile(title,subTitle,color){
     return ListTile(
             leading: CircleAvatar(
@@ -50,5 +54,5 @@ class MyApp extends StatelessWidget{
             subtitle: Text(subTitle,),
             trailing: Icon(Icons.call),
     );
-  }
+  }//ListTile
 }
