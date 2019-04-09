5624e0422ed784877df8e30664f51e720c941a5b
Nawsher Ali <40803579+Nawsher-Ali@users.noreply.github.com>
2019-04-09 07:05:30 +0600

    Update main.dart

--- 02_appBar_with_textstyle/lib/main.dart
+++ 02_appBar_with_textstyle/lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main(){
+  
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner:false,
+      debugShowCheckedModeBanner:false,//this line is used for remove debug banner from top right Corner
       home:Scaffold(
+        
         backgroundColor: Colors.deepPurple,
+        
         appBar: AppBar(title:Text('I am appBar'),backgroundColor: Colors.blue[900],),
+        
         body:Center(
           child: Text(
             'Hello I am body',
             style: TextStyle(fontSize: 45,color:Colors.white),
           ),
-        )
-      )
-    )
+        )//body
+        
+      )//Scaffold
+    )//MaterialApp
   );
 }
