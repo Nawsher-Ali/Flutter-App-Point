f681f6436672d85fe0903e6f4e0aeaf51bd6f3ff
Nawsher Ali <40803579+Nawsher-Ali@users.noreply.github.com>
2019-04-09 07:19:17 +0600

    Update main.dart

--- 06_row_widget/lib/main.dart
+++ 06_row_widget/lib/main.dart
@@ -11,6 +11,7 @@ void main(){
 }
 
 class MyApp extends StatelessWidget{
+  
   @override
   build(BuildContext context){
     return Scaffold(
@@ -18,37 +19,43 @@ class MyApp extends StatelessWidget{
       appBar:AppBar(
         title:Text('Row Widget Exploring'),
         backgroundColor:Colors.blue[900],
-      ),
+      ),//AppBar
 
       body:Center(
-        child:Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:<Widget> [
-              Container(
-                margin: EdgeInsets.only(right: 7),
-                color: Colors.blue[500],
-                height:300,
-                width:100,
-                child:Icon(Icons.laptop_windows,size:50,color:Colors.white)
-              ),
-              Container(
-                margin: EdgeInsets.only(right: 7),
-                color: Colors.blue[500],
-                height:300,
-                width:100,
-                child:Icon(Icons.favorite_border,size:50,color:Colors.white)
-              ),
-              Container(
-                color: Colors.blue[500],
-                height:300,
-                width:100,
-                child:Icon(Icons.person_pin_circle,size:50,color:Colors.white)
-              )
-            ]
-          ),
-        )
-      )
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:<Widget> [
+            
+            //Container 1
+            Container(
+              margin: EdgeInsets.only(right: 7),
+              color: Colors.blue[500],
+              height:300,
+              width:100,
+              child:Icon(Icons.laptop_windows,size:50,color:Colors.white)
+            ),
+            
+            //Container 2
+            Container(
+              margin: EdgeInsets.only(right: 7),
+              color: Colors.blue[500],
+              height:300,
+              width:100,
+              child:Icon(Icons.favorite_border,size:50,color:Colors.white)
+            ),
+            
+            //Container 3
+            Container(
+              color: Colors.blue[500],
+              height:300,
+              width:100,
+              child:Icon(Icons.person_pin_circle,size:50,color:Colors.white)
+            )
+            
+          ]
+        )//Row
+        
+      )//Center
     );
   }
 }
