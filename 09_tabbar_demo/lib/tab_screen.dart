c7e8978654a54ddfd9744bccba1883b9da89fd04
Nawsher Ali <40803579+Nawsher-Ali@users.noreply.github.com>
2019-04-09 07:57:20 +0600

    Update tab_screen.dart

--- 09_tabbar_demo/lib/tab_screen.dart
+++ 09_tabbar_demo/lib/tab_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
 class GetBodyWidget extends StatelessWidget{
+  
   Icon icon;
   GetBodyWidget(this.icon);
 
   @override
    build(BuildContext context) {
+     
     return  Container(
         child:Center(
           child: icon,
         )
-      );
+    );
   }
+  
 }
