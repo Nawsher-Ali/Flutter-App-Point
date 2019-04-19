a8bf1580925899193aa030816615ca0c3d73c25f
Nawsher Ali <nawsher321@gmail.com>
2019-04-19 18:34:47 +0600

    added placeholder project

---
+++ placeholder_demo/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      title:'placeholder demo',
+      debugShowCheckedModeBanner:false,
+      home:MyApp(),
+    )
+  );
+}
+
+class MyApp extends StatelessWidget{
+
+  @override
+  build(BuildContext context){
+    return Scaffold(
+      
+      appBar: AppBar(
+        title:Text('Placeholder Widget'),
+        backgroundColor:Color(0xFF0A3D62)
+      ),
+
+      body:Placeholder(
+        color:Colors.redAccent,
+        // fallbackHeight: 200,
+        // fallbackWidth: 200,
+        strokeWidth:10
+      )
+    );  
+  }
+}
