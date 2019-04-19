6e93e7764f2dae348698e75e6d5ddd773a303fed
Nawsher Ali <nawsher321@gmail.com>
2019-04-19 19:55:03 +0600

    Added spaceholder project

---
+++ spacer_demo/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(
+    MaterialApp(
+      title:'Spacer Widget',
+      debugShowCheckedModeBanner:false,
+      home:MyApp(),
+    )
+  );
+}
+
+class MyApp extends StatelessWidget{
+
+  @override
+  build(BuildContext context){
+    return Scaffold(
+
+      appBar:AppBar(
+        title:Text('Spacer Widget'),
+        backgroundColor: Color(0xFFAE1438),
+      ),
+
+      body:Center(
+        child: Row(
+          children: <Widget>[
+            Text('one',style:TextStyle(fontSize:18,fontWeight:FontWeight.bold)
+            ),
+            Spacer(
+              flex:3
+            ),
+            Text('two',style:TextStyle(fontSize:18,fontWeight:FontWeight.bold)),
+            Spacer(
+              flex:1
+            ),
+            Text('three',style:TextStyle(fontSize:18,fontWeight:FontWeight.bold)),
+          ],
+        ),
+      )
+    );  
+  }
+}
